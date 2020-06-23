32ddd38b78295de3086ad94ed351ca6e5d1642d2
KaranveerB <52545097+KaranveerB@users.noreply.github.com>
2020-06-22 23:38:47 -0700

    Change font to DejaVu + Hack Nerd Font

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int gappx     = 2;        /* gaps between windows */
 static const unsigned int snap      = 12;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro Medium:size=9:antialias=true:autohint=true" };
+static const char *fonts[]          = { "DejaVu Sans:size=9:antialias=true:autohint=true", "Hack Nerd Font:size=9:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Source Code Pro Medmium:size=9:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
