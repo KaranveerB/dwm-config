272c3d661016849e9e28ae9c049103f42aa661dd
KaranveerB <52545097+KaranveerB@users.noreply.github.com>
2020-06-24 05:37:05 -0700

    Use pywal for colours

--- config.h
+++ config.h
@@ -17,11 +17,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
+#include "/home/<USER>/.cache/wal/colors-wal-dwm.h"
 
 /* audio */
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
