219cce4a79641d9a512bdf28b909dd08453d659b
KaranveerB <52545097+KaranveerB@users.noreply.github.com>
2022-04-03 13:48:39 -0700

    Make theme modular

--- config.h
+++ config.h
@@ -1,35 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 #include <X11/XF86keysym.h>
-
-/* appearance */
-static const unsigned int borderpx  = 3;              /* border pixel of windows */
-static const unsigned int gappx     = 2;              /* gaps between windows */
-static const unsigned int snap      = 24;             /* snap pixel */
-static const int showbar            = 1;              /* 0 means no bar */
-static const int topbar             = 1;              /* 0 means bottom bar */
-static const int usealtbar          = 1;              /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "Polybar";      /* alt bar class name */
-static const char *altbarcmd        = "$HOME/tmp.sh"; /* alt bar launch command */
-static const char *fonts[]          = { "DejaVu Sans Mono:size=11:antialias=true:autohint=true",
-					"Symbols Nerd Font:size=10:antialias=true:autohint=true" };
-static const char dmenufont[]       = "DejaVu Sans Mono:size=11:antialias=true:autohint=true";
-
-static const char norm_fg[]         = "#a2dfef";
-static const char norm_bg[]         = "#010311";
-static const char norm_border[]     = "#719ca7";
-static const char sel_fg[]          = "#18aaec";
-static const char sel_bg[]          = "#010311";
-static const char sel_border[]      = "#a2dfef";
-
-static const char *colors[][3]      = {
-    /*               fg           bg         border                         */
-    [SchemeNorm] = { norm_fg,     norm_bg,   norm_border }, // unfocused wins
-    [SchemeSel]  = { sel_fg,      sel_bg,    sel_border },  // the focused win
-};
-
-/* tagging */
-static const char *tags[] = { "⭘", "⭘", "⭘", "⭘", "⭘", "⭘", "⭘", "⭘", "⭘" };
+#include "theme.h"
 
 static const Rule rules[] = {
 	/* xprop(1):
